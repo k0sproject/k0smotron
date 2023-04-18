31dd990e7909fce1dc0d6f47665c950bc957a214
Jussi Nummelin <jnummelin@mirantis.com>
2023-04-18 14:25:06 +0300

    Use dynamic Patch to create or update the objects instead of custom logic.
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- internal/exec/exec.go
+++ internal/exec/exec.go
@@ -19,6 +19,7 @@ package exec
 import (
 	"bytes"
 	"context"
+	"fmt"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
@@ -48,14 +49,15 @@ func PodExecCmdOutput(ctx context.Context, client kubernetes.Interface, config *
 		return "", err
 	}
 
-	var b bytes.Buffer
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
 	err = exec.StreamWithContext(ctx, remotecommand.StreamOptions{
-		Stdout: &b,
-		Stderr: &b,
+		Stdout: &stdout,
+		Stderr: &stderr,
 	})
 	if err != nil {
-		return b.String(), err
+		return stdout.String(), fmt.Errorf("failed to exec command: %w, stderr: %s", err, stderr.String())
 	}
 
-	return b.String(), nil
+	return stdout.String(), nil
 }
