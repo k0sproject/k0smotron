447019469cdb276585dca10a471d8b9788d6046f
Jussi Nummelin <jnummelin@mirantis.com>
2023-10-04 10:55:58 +0300

    Use rig remoteFS for file operations and thus get rid of k0sctl dependency
    Signed-off-by: Jussi Nummelin <jnummelin@mirantis.com>

--- go.mod
+++ go.mod
@@ -1,15 +1,13 @@
 module github.com/k0sproject/k0smotron
 
-go 1.20
+go 1.21
 
 require (
-	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/go-logr/logr v1.2.4
 	github.com/google/uuid v1.3.0
 	github.com/imdario/mergo v0.3.16
 	github.com/k0sproject/k0s v1.27.2-0.20230504131248-94378e521a29
-	github.com/k0sproject/k0sctl v0.15.2
-	github.com/k0sproject/rig v0.11.0
+	github.com/k0sproject/rig v0.14.0
 	github.com/onsi/ginkgo/v2 v2.12.1
 	github.com/onsi/gomega v1.28.0
 	github.com/stretchr/testify v1.8.4
@@ -54,7 +52,7 @@ require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	golang.org/x/crypto v0.12.0
+	golang.org/x/crypto v0.13.0
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/tools v0.12.0 // indirect
 	gotest.tools/v3 v3.4.0 // indirect
@@ -67,9 +65,8 @@ require (
 	github.com/ChrisTrenkamp/goxpath v0.0.0-20210404020558-97928f7e12b6 // indirect
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
-	github.com/alessio/shellescape v1.4.1 // indirect
-	github.com/bmatcuk/doublestar/v4 v4.6.0 // indirect
-	github.com/creasty/defaults v1.6.0 // indirect
+	github.com/alessio/shellescape v1.4.2 // indirect
+	github.com/creasty/defaults v1.7.0 // indirect
 	github.com/davidmz/go-pageant v1.0.2 // indirect
 	github.com/emicklei/go-restful/v3 v3.10.1 // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
@@ -78,10 +75,6 @@ require (
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.1 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
-	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.13.0 // indirect
 	github.com/gobuffalo/flect v1.0.2 // indirect
 	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
@@ -92,7 +85,6 @@ require (
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
-	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
 	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
 	github.com/jcmturner/gofork v1.7.6 // indirect
@@ -101,11 +93,8 @@ require (
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/k0sproject/dig v0.2.0 // indirect
-	github.com/k0sproject/version v0.3.0 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/kevinburke/ssh_config v1.2.0 // indirect
-	github.com/leodido/go-urn v1.2.3 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/masterzen/simplexml v0.0.0-20190410153822-31eea3082786 // indirect
 	github.com/masterzen/winrm v0.0.0-20220917170901-b07f6cb0598d // indirect
@@ -126,8 +115,8 @@ require (
 	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/oauth2 v0.10.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/term v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
--- internal/controller/infrastructure/provisioner.go
+++ internal/controller/infrastructure/provisioner.go
@@ -20,26 +20,15 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"os"
+	"io"
 	"path/filepath"
-	"strconv"
-	"time"
 
-	"github.com/avast/retry-go"
 	"github.com/go-logr/logr"
-	k0sctl "github.com/k0sproject/k0sctl/pkg/apis/k0sctl.k0sproject.io/v1beta1/cluster"
 	api "github.com/k0sproject/k0smotron/api/infrastructure/v1beta1"
 	"github.com/k0sproject/k0smotron/internal/cloudinit"
 	"github.com/k0sproject/rig"
-	"github.com/k0sproject/rig/exec"
+	"github.com/k0sproject/rig/pkg/rigfs"
 	"gopkg.in/yaml.v3"
-
-	// anonymous import is needed to load the os configurers
-	_ "github.com/k0sproject/k0sctl/configurer"
-	// anonymous import is needed to load the os configurers
-	_ "github.com/k0sproject/k0sctl/configurer/linux"
-	// anonymous import is needed to load the os configurers
-	_ "github.com/k0sproject/k0sctl/configurer/linux/enterpriselinux"
 )
 
 type Provisioner struct {
@@ -63,50 +52,37 @@ func (p *Provisioner) Provision(ctx context.Context) error {
 		return fmt.Errorf("failed to parse bootstrap data: %w", err)
 	}
 
-	// Dump key to a temporary file
-	f, err := os.CreateTemp("", "k0smotron-ssh-key")
+	authM, err := rig.ParseSSHPrivateKey([]byte(p.sshKey), rig.DefaultPasswordCallback)
 	if err != nil {
-		return fmt.Errorf("failed to create temporary key file: %w", err)
+		return fmt.Errorf("failed to parse ssh key: %w", err)
 	}
-	defer os.Remove(f.Name())
 
-	if _, err := f.Write(p.sshKey); err != nil {
-		return fmt.Errorf("failed to write key to temporary file: %w", err)
-	}
-	f.Close()
-
-	keyFile := f.Name()
-
-	connection := rig.Connection{
+	connection := &rig.Connection{
 		SSH: &rig.SSH{
-			Address: p.machine.Spec.Address,
-			Port:    p.machine.Spec.Port,
-			User:    p.machine.Spec.User,
-			KeyPath: &keyFile,
+			Address:     p.machine.Spec.Address,
+			Port:        p.machine.Spec.Port,
+			User:        p.machine.Spec.User,
+			AuthMethods: authM,
 		},
 	}
 
-	// Wrap the connection in k0sctl host to get OS resolving etc.
-	host := &k0sctl.Host{
-		Connection: connection,
-	}
-
-	if err := p.connect(host); err != nil {
+	if err := connection.Connect(); err != nil {
 		return fmt.Errorf("failed to connect to host: %w", err)
 	}
 
-	defer host.Disconnect()
+	defer connection.Disconnect()
 
 	// Write files first
 	for _, file := range cloudInit.Files {
-		if err := p.uploadFile(host, file); err != nil {
+		if err := p.uploadFile(connection, file); err != nil {
 			return fmt.Errorf("failed to upload file: %w", err)
 		}
 	}
 
 	// Execute the bootstrap script commands
 	for _, cmd := range cloudInit.RunCmds {
-		output, err := host.ExecOutput(cmd)
+
+		output, err := connection.ExecOutput(cmd)
 		if err != nil {
 			p.log.Error(err, "failed to run command", "output", output)
 			return fmt.Errorf("failed to run command: %w", err)
@@ -114,61 +90,33 @@ func (p *Provisioner) Provision(ctx context.Context) error {
 	}
 
 	// Check for sentinel file
-	if !host.Configurer.FileExist(host, "/run/cluster-api/bootstrap-success.complete") {
+	fsys := connection.SudoFsys()
+	if _, err := fsys.Stat("/run/cluster-api/bootstrap-success.complete"); err != nil {
 		return errors.New("bootstrap sentinel file not found")
 	}
 
 	return nil
 }
 
-const retries = uint(60)
-
-func (p *Provisioner) uploadFile(h *k0sctl.Host, file cloudinit.File) error {
-
+func (p *Provisioner) uploadFile(conn *rig.Connection, file cloudinit.File) error {
+	fsys := conn.SudoFsys()
 	// Ensure base dir exists for target
 	dir := filepath.Dir(file.Path)
-	if err := h.Configurer.MkDir(h, dir, exec.Sudo(h)); err != nil {
+	perms, err := file.PermissionsAsInt()
+	if err != nil {
+		return fmt.Errorf("failed to parse permissions: %w", err)
+	}
+	if err := fsys.MkDirAll(dir, rigfs.FileMode(perms)); err != nil {
 		return fmt.Errorf("failed to create directory: %w", err)
 	}
 
-	if err := h.Configurer.WriteFile(h, file.Path, file.Content, file.Permissions); err != nil {
-		return fmt.Errorf("failed to write file: %w", err)
-	}
-
-	return nil
-}
-
-func (p *Provisioner) connect(h *k0sctl.Host) error {
-	err := retry.Do(
-		func() error {
-			h.SSH.SetDefaults()
-			return h.Connect()
-		},
-		retry.OnRetry(
-			func(n uint, err error) {
-				attNr := strconv.FormatUint(uint64(n+1), 10)
-				p.log.Error(err, "failed to connect", "host", h.String(), "attempt", attNr)
-			},
-		),
-		retry.RetryIf(
-			func(err error) bool {
-				return !errors.Is(err, rig.ErrCantConnect)
-			},
-		),
-		retry.DelayType(retry.CombineDelay(retry.FixedDelay, retry.RandomDelay)),
-		retry.MaxJitter(time.Second*2),
-		retry.Delay(time.Second*3),
-		retry.Attempts(retries),
-		retry.LastErrorOnly(true),
-	)
-
+	destFile, err := fsys.OpenFile(file.Path, rigfs.ModeCreate, rigfs.ModeReadWrite)
 	if err != nil {
-		return fmt.Errorf("failed to connect to host: %w", err)
+		return fmt.Errorf("failed to open remote file for writing: %w", err)
 	}
-
-	// Resolve the OS type etc.
-	if err := h.ResolveConfigurer(); err != nil {
-		return fmt.Errorf("failed to resolve host OS: %w", err)
+	defer destFile.Close()
+	if _, err := io.WriteString(destFile, file.Content); err != nil {
+		return fmt.Errorf("failed to write to remote file: %w", err)
 	}
 
 	return nil
