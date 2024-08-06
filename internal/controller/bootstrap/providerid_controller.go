865565c6c11bdf4a25c75e6ab82536065be058d5
Alexey Makhov <amakhov@mirantis.com>
2024-08-06 09:25:49 +0300

    Check for CAPI crds to be installed before setup
    Signed-off-by: Alexey Makhov <amakhov@mirantis.com>

--- internal/controller/bootstrap/providerid_controller.go
+++ internal/controller/bootstrap/providerid_controller.go
@@ -8,6 +8,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/util/retry"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	capiutil "sigs.k8s.io/cluster-api/util"
@@ -20,7 +21,8 @@ import (
 
 type ProviderIDController struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme    *runtime.Scheme
+	ClientSet *kubernetes.Clientset
 }
 
 func (p *ProviderIDController) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
@@ -88,6 +90,15 @@ func (p *ProviderIDController) Reconcile(ctx context.Context, req ctrl.Request) 
 }
 
 func (p *ProviderIDController) SetupWithManager(mgr ctrl.Manager) error {
+	apiResources, err := p.ClientSet.Discovery().ServerResourcesForGroupVersion(clusterv1.GroupVersion.String())
+	if err != nil && !apierrors.IsNotFound(err) {
+		return err
+	}
+	if apiResources == nil {
+		log.Log.Info("CAPI crds are not installed yet, skipping initializing providerID controller")
+		return nil
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&clusterv1.Machine{}).
 		Complete(p)
