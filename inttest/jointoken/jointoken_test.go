0ea563e9a2f5c9415f0ba34d1ccbd06c1d4c23ed
Alexey Makhov <amakhov@mirantis.com>
2024-12-04 17:39:25 +0200

    Admission webhook
    Signed-off-by: Alexey Makhov <amakhov@mirantis.com>

--- inttest/jointoken/jointoken_test.go
+++ inttest/jointoken/jointoken_test.go
@@ -38,7 +38,7 @@ type JoinTokenSuite struct {
 
 func (s *JoinTokenSuite) TestK0sGetsUp() {
 	s.T().Log("starting k0s")
-	s.Require().NoError(s.InitController(0, "--disable-components=konnectivity-server,metrics-server"))
+	s.Require().NoError(s.InitController(0, "--disable-components=metrics-server"))
 	s.Require().NoError(s.RunWorkers())
 
 	kc, err := s.KubeClient(s.ControllerNode(0))
