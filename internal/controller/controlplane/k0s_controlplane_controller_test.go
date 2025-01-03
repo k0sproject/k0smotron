9ba00f80be10c6eeaf471af664ec1f5e8055092f
Alexey Makhov <amakhov@mirantis.com>
2025-01-03 10:28:06 +0200

    Updating control nodes one by one. Test and fix
    Signed-off-by: Alexey Makhov <amakhov@mirantis.com>

--- internal/controller/controlplane/k0s_controlplane_controller_test.go
+++ internal/controller/controlplane/k0s_controlplane_controller_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package controlplane
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -115,6 +116,98 @@ func TestK0sConfigEnrichment(t *testing.T) {
 		t.Run("", func(t *testing.T) {
 			actual, err := enrichK0sConfigWithClusterData(tc.cluster, tc.kcp.Spec.K0sConfigSpec.K0s)
 			require.NoError(t, err)
+			require.Equal(t, tc.want, actual)
+		})
+	}
+}
+
+func Test_machineName(t *testing.T) {
+	var testCases = []struct {
+		replicas        int32
+		machineToDelete map[string]bool
+		desiredMachines map[string]bool
+		want            string
+	}{
+		{
+			replicas:        3,
+			machineToDelete: nil,
+			desiredMachines: map[string]bool{},
+			want:            "test-0",
+		},
+		{
+			replicas:        3,
+			machineToDelete: nil,
+			desiredMachines: map[string]bool{
+				"test-1": true,
+			},
+			want: "test-0",
+		},
+		{
+			replicas: 3,
+			machineToDelete: map[string]bool{
+				"test-0": true,
+				"test-1": true,
+				"test-2": true,
+			},
+			desiredMachines: map[string]bool{
+				"test-3": true,
+			},
+			want: "test-4",
+		},
+		{
+			replicas: 3,
+			machineToDelete: map[string]bool{
+				"test-3": true,
+				"test-4": true,
+				"test-5": true,
+			},
+			desiredMachines: map[string]bool{},
+			want:            "test-0",
+		},
+		{
+			replicas: 3,
+			machineToDelete: map[string]bool{
+				"test-4": true,
+				"test-5": true,
+			},
+			desiredMachines: map[string]bool{
+				"test-0": true,
+			},
+			want: "test-1",
+		},
+		{
+			replicas: 3,
+			machineToDelete: map[string]bool{
+				"test-5": true,
+			},
+			desiredMachines: map[string]bool{
+				"test-0": true,
+				"test-1": true,
+			},
+			want: "test-2",
+		},
+		{
+			replicas:        3,
+			machineToDelete: nil,
+			desiredMachines: map[string]bool{
+				"test-1": true,
+				"test-2": true,
+			},
+			want: "test-0",
+		},
+	}
+
+	for _, tc := range testCases {
+		kcp := &v1beta1.K0sControlPlane{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: v1beta1.K0sControlPlaneSpec{
+				Replicas: tc.replicas,
+			},
+		}
+		t.Run("", func(t *testing.T) {
+			actual := machineName(kcp, tc.machineToDelete, tc.desiredMachines)
 			require.Equal(t, tc.want, actual)
 		})
 	}
