5d9500948989c9c732a3ea3726827b8556096226
makhov <amakhov@mirantis.com>
2025-10-22 15:30:50 +0300

    Test fix after dependency version bump
    Signed-off-by: Alexey Makhov <amakhov@mirantis.com>
    Signed-off-by: makhov <amakhov@mirantis.com>

--- e2e/workload_cluster_recreate_delete_first_upgrade_test.go
+++ e2e/workload_cluster_recreate_delete_first_upgrade_test.go
@@ -63,7 +63,7 @@ func workloadClusterRecreateDeleteFirstUpgradeSpec(t *testing.T) {
 
 		Namespace:                namespace.Name,
 		ClusterName:              clusterName,
-		KubernetesVersion:        e2eConfig.GetVariable(KubernetesVersion),
+		KubernetesVersion:        e2eConfig.MustGetVariable(KubernetesVersion),
 		ControlPlaneMachineCount: ptr.To[int64](3),
 		// TODO: make infra provider configurable
 		InfrastructureProvider: "docker",
@@ -98,6 +98,7 @@ func workloadClusterRecreateDeleteFirstUpgradeSpec(t *testing.T) {
 			cluster,
 			util.GetInterval(e2eConfig, testName, "wait-delete-cluster"),
 			skipCleanup,
+			clusterctlConfigPath,
 		)
 	}()
 
@@ -112,7 +113,7 @@ func workloadClusterRecreateDeleteFirstUpgradeSpec(t *testing.T) {
 		ClusterProxy:                     bootstrapClusterProxy,
 		Cluster:                          cluster,
 		ControlPlane:                     controlPlane,
-		KubernetesUpgradeVersion:         e2eConfig.GetVariable(KubernetesVersionFirstUpgradeTo),
+		KubernetesUpgradeVersion:         e2eConfig.MustGetVariable(KubernetesVersionFirstUpgradeTo),
 		WaitForKubeProxyUpgradeInterval:  util.GetInterval(e2eConfig, testName, "wait-kube-proxy-upgrade"),
 		WaitForControlPlaneReadyInterval: util.GetInterval(e2eConfig, testName, "wait-control-plane"),
 	})
@@ -123,7 +124,7 @@ func workloadClusterRecreateDeleteFirstUpgradeSpec(t *testing.T) {
 		ClusterProxy:                     bootstrapClusterProxy,
 		Cluster:                          cluster,
 		ControlPlane:                     controlPlane,
-		KubernetesUpgradeVersion:         e2eConfig.GetVariable(KubernetesVersionSecondUpgradeTo),
+		KubernetesUpgradeVersion:         e2eConfig.MustGetVariable(KubernetesVersionSecondUpgradeTo),
 		WaitForKubeProxyUpgradeInterval:  util.GetInterval(e2eConfig, testName, "wait-kube-proxy-upgrade"),
 		WaitForControlPlaneReadyInterval: util.GetInterval(e2eConfig, testName, "wait-control-plane"),
 	})
