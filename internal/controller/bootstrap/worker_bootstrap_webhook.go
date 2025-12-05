f8d220a2c585a43bf1ee172969155c937072bd3b
apedriza <adripedriza@gmail.com>
2025-12-05 17:45:52 +0100

    Add validating webhook for k0sworkerconfig
    Signed-off-by: apedriza <adripedriza@gmail.com>

---
+++ internal/controller/bootstrap/worker_bootstrap_webhook.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package bootstrap
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/k0sproject/k0smotron/api/bootstrap/v1beta1"
+	bootstrapv1 "github.com/k0sproject/k0smotron/api/bootstrap/v1beta1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// +kubebuilder:webhook:path=/validate-bootstrap-cluster-x-k8s-io-v1beta1-k0sworkerconfig,mutating=false,failurePolicy=fail,sideEffects=None,groups=bootstrap.cluster.x-k8s.io,resources=k0sworkerconfigs,verbs=create;update,versions=v1beta1,name=validate-k0sworkerconfig-v1beta1.k0smotron.io,admissionReviewVersions=v1
+
+// K0sWorkerConfigValidator implements a validation webhook for K0sWorkerConfig.
+type K0sWorkerConfigValidator struct{}
+
+var _ webhook.CustomValidator = &K0sWorkerConfigValidator{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type.
+func (v *K0sWorkerConfigValidator) ValidateCreate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
+	c, ok := obj.(*bootstrapv1.K0sWorkerConfig)
+	if !ok {
+		return nil, apierrors.NewBadRequest(fmt.Sprintf("expected a K0sWorkerConfig but got a %T", obj))
+	}
+
+	return nil, v.validate(c.Spec, c.Name)
+}
+
+// ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type.
+func (v *K0sWorkerConfigValidator) ValidateUpdate(_ context.Context, _, newObj runtime.Object) (admission.Warnings, error) {
+	newC, ok := newObj.(*bootstrapv1.K0sWorkerConfig)
+	if !ok {
+		return nil, apierrors.NewBadRequest(fmt.Sprintf("expected a K0sWorkerConfig but got a %T", newObj))
+	}
+
+	return nil, v.validate(newC.Spec, newC.Name)
+}
+
+// ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type.
+func (v *K0sWorkerConfigValidator) ValidateDelete(_ context.Context, _ runtime.Object) (admission.Warnings, error) {
+	return nil, nil
+}
+
+func (v *K0sWorkerConfigValidator) validate(c bootstrapv1.K0sWorkerConfigSpec, name string) error {
+	allErrs := c.Validate(field.NewPath("spec"))
+
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(bootstrapv1.GroupVersion.WithKind("K0sWorkerConfig").GroupKind(), name, allErrs)
+}
+
+// SetupK0sWorkerConfigWebhookWithManager registers the webhook for K0sWorkerConfig in the manager.
+func (v *K0sWorkerConfigValidator) SetupK0sWorkerConfigWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(&v1beta1.K0sWorkerConfig{}).
+		WithValidator(v).
+		Complete()
+}
---
+++ internal/controller/bootstrap/worker_bootstrap_webhook_test.go
@@ -0,0 +1,231 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package bootstrap
+
+import (
+	"context"
+	"testing"
+
+	bootstrapv1 "github.com/k0sproject/k0smotron/api/bootstrap/v1beta1"
+	"github.com/k0sproject/k0smotron/internal/provisioner"
+	"github.com/stretchr/testify/require"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+func TestK0sWorkerConfigValidate(t *testing.T) {
+	testCases := []struct {
+		name             string
+		in               *bootstrapv1.K0sWorkerConfig
+		expectedWarnings admission.Warnings
+		expectingError   bool
+	}{
+		{
+			name: "valid config passes validation",
+			in: &bootstrapv1.K0sWorkerConfig{
+				Spec: bootstrapv1.K0sWorkerConfigSpec{
+					Version: "v1.27.4+k0s.0",
+					Files: []bootstrapv1.File{
+						{
+							ContentFrom: &bootstrapv1.ContentSource{
+								SecretRef: &bootstrapv1.ContentSourceRef{
+									Name: "my-secret",
+									Key:  "my-key",
+								},
+							},
+						},
+						{
+							File: provisioner.File{
+								Path:    "/one/path/to/file",
+								Content: "some-content",
+							},
+						},
+						{
+							File: provisioner.File{
+								Path:    "/another/path/to/file",
+								Content: "some-content",
+							},
+						},
+					},
+				},
+			},
+			expectedWarnings: nil,
+			expectingError:   false,
+		},
+		{
+			name: "err for unsupported k0s version",
+			in: &bootstrapv1.K0sWorkerConfig{
+				Spec: bootstrapv1.K0sWorkerConfigSpec{
+					Version: "v1.27.4-k0s.0",
+				},
+			},
+			expectedWarnings: nil,
+			expectingError:   true,
+		},
+		{
+			name: "err for invalid files declared in config: content and contentFrom conflict",
+			in: &bootstrapv1.K0sWorkerConfig{
+				Spec: bootstrapv1.K0sWorkerConfigSpec{
+					Version: "v1.27.4+k0s.0",
+					Files: []bootstrapv1.File{
+						{
+							File: provisioner.File{
+								Content: "some-content",
+							},
+							ContentFrom: &bootstrapv1.ContentSource{
+								SecretRef: &bootstrapv1.ContentSourceRef{
+									Name: "my-secret",
+									Key:  "my-key",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedWarnings: nil,
+			expectingError:   true,
+		},
+		{
+			name: "err for invalid files declared in config: not content",
+			in: &bootstrapv1.K0sWorkerConfig{
+				Spec: bootstrapv1.K0sWorkerConfigSpec{
+					Version: "v1.27.4+k0s.0",
+					Files: []bootstrapv1.File{
+						{
+							File: provisioner.File{
+								Content: "",
+							},
+						},
+					},
+				},
+			},
+			expectedWarnings: nil,
+			expectingError:   true,
+		},
+		{
+			name: "err for invalid files declared in config: contentFrom configmap and secret conflict",
+			in: &bootstrapv1.K0sWorkerConfig{
+				Spec: bootstrapv1.K0sWorkerConfigSpec{
+					Version: "v1.27.4+k0s.0",
+					Files: []bootstrapv1.File{
+						{
+							ContentFrom: &bootstrapv1.ContentSource{
+								SecretRef: &bootstrapv1.ContentSourceRef{
+									Name: "my-secret",
+									Key:  "my-key",
+								},
+								ConfigMapRef: &bootstrapv1.ContentSourceRef{
+									Name: "my-configmap",
+									Key:  "my-key",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedWarnings: nil,
+			expectingError:   true,
+		},
+		{
+			name: "err for invalid files declared in config: contentFrom configmap name missing",
+			in: &bootstrapv1.K0sWorkerConfig{
+				Spec: bootstrapv1.K0sWorkerConfigSpec{
+					Version: "v1.27.4+k0s.0",
+					Files: []bootstrapv1.File{
+						{
+							ContentFrom: &bootstrapv1.ContentSource{
+								ConfigMapRef: &bootstrapv1.ContentSourceRef{
+									Name: "",
+									Key:  "my-key",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedWarnings: nil,
+			expectingError:   true,
+		},
+		{
+			name: "err for invalid files declared in config: contentFrom secret name missing",
+			in: &bootstrapv1.K0sWorkerConfig{
+				Spec: bootstrapv1.K0sWorkerConfigSpec{
+					Version: "v1.27.4+k0s.0",
+					Files: []bootstrapv1.File{
+						{
+							ContentFrom: &bootstrapv1.ContentSource{
+								SecretRef: &bootstrapv1.ContentSourceRef{
+									Name: "",
+									Key:  "my-key",
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedWarnings: nil,
+			expectingError:   true,
+		},
+		{
+			name: "err for invalid files declared in config: contentFrom secret name missing",
+			in: &bootstrapv1.K0sWorkerConfig{
+				Spec: bootstrapv1.K0sWorkerConfigSpec{
+					Version: "v1.27.4+k0s.0",
+					Files: []bootstrapv1.File{
+						{
+							File: provisioner.File{
+								Path: "same-path",
+							},
+						},
+						{
+							File: provisioner.File{
+								Path: "same-path",
+							},
+						},
+						{
+							File: provisioner.File{
+								Path: "same-path",
+							},
+						},
+					},
+				},
+			},
+			expectedWarnings: nil,
+			expectingError:   true,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			validator := &K0sWorkerConfigValidator{}
+			warnings, err := validator.ValidateCreate(context.Background(), tc.in)
+			if tc.expectingError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			require.Empty(t, warnings)
+
+			warnings, err = validator.ValidateUpdate(context.Background(), nil, tc.in)
+			if tc.expectingError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			require.Empty(t, warnings)
+
+		})
+	}
+}
