de3478fa8c2d4edb6400a8781e601bee8e62bf2d
apedriza <adripedriza@gmail.com>
2026-03-09 16:39:46 +0100

    Fix conversion for non-supported fields
    Signed-off-by: apedriza <adripedriza@gmail.com>

--- api/controlplane/v1beta1/k0smotron_conversion.go
+++ api/controlplane/v1beta1/k0smotron_conversion.go
@@ -1,11 +1,13 @@
 package v1beta1
 
 import (
+	"fmt"
+
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 
 	"github.com/k0sproject/k0smotron/api/controlplane/v1beta2"
-	kmcv1 "github.com/k0sproject/k0smotron/api/k0smotron.io/v1beta1"
+	kmcv1beta1 "github.com/k0sproject/k0smotron/api/k0smotron.io/v1beta1"
 )
 
 // ConvertTo converts this version (v1beta2) to the hub version (v1beta2 - self).
@@ -13,7 +15,7 @@ func (k *K0smotronControlPlane) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*v1beta2.K0smotronControlPlane)
 	dst.ObjectMeta = *k.ObjectMeta.DeepCopy()
 
-	dst.Spec = k.Spec
+	dst.Spec = kmcv1beta1.ClusterSpecToV2(k.Spec)
 	dst.Status = v1beta2.K0smotronControlPlaneStatus{
 		Initialization: v1beta2.Initialization{
 			ControlPlaneInitialized: &k.Status.Initialized,
@@ -37,7 +39,7 @@ func (k *K0smotronControlPlaneTemplate) ConvertTo(dstRaw conversion.Hub) error {
 	dst.Spec = v1beta2.K0smotronControlPlaneTemplateSpec{
 		Template: v1beta2.K0smotronControlPlaneTemplateResource{
 			ObjectMeta: k.Spec.Template.ObjectMeta,
-			Spec:       kmcv1.ClusterSpecToV2(k.Spec.Template.Spec),
+			Spec:       kmcv1beta1.ClusterSpecToV2(k.Spec.Template.Spec),
 		},
 	}
 	return nil
@@ -47,7 +49,7 @@ func (k *K0smotronControlPlaneTemplate) ConvertTo(dstRaw conversion.Hub) error {
 func (k *K0smotronControlPlaneTemplate) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*v1beta2.K0smotronControlPlaneTemplate)
 	k.ObjectMeta = *src.ObjectMeta.DeepCopy()
-	spec, err := kmcv1.ClusterSpecFromV2(src.Spec.Template.Spec)
+	spec, err := kmcv1beta1.ClusterSpecFromV2(src.Spec.Template.Spec)
 	if err != nil {
 		return err
 	}
@@ -64,7 +66,11 @@ func (k *K0smotronControlPlaneTemplate) ConvertFrom(srcRaw conversion.Hub) error
 func (k *K0smotronControlPlane) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*v1beta2.K0smotronControlPlane)
 	k.ObjectMeta = *src.ObjectMeta.DeepCopy()
-	k.Spec = (src.Spec)
+	v1beta1Spec, err := kmcv1beta1.ClusterSpecFromV2(src.Spec)
+	if err != nil {
+		return fmt.Errorf("failed to convert K0smotronControlPlane.Spec: %w", err)
+	}
+	k.Spec = v1beta1Spec
 	k.Status = K0smotronControlPlaneStatus{
 		Ready:       ptr.Deref(src.Status.ReadyReplicas, 0) > 0,
 		Initialized: ptr.Deref(src.Status.Initialization.ControlPlaneInitialized, false),
