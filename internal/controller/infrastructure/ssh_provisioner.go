2779b34cfe97bab14cf339344fb72b9813d947fd
Alexey Makhov <amakhov@mirantis.com>
2026-04-14 15:33:44 +0300

    Fix ssh host key mismatch on VM recreation (#1422)
    * Fix ssh host key mismatch on VM recreation.
    
    Signed-off-by: makhov <amakhov@mirantis.com>
    
    * Fix ssh host key mismatch on VM recreation. Remove SSH_KNOWN_HOSTS env var
    
    Signed-off-by: makhov <amakhov@mirantis.com>
    
    * Fix ssh host key mismatch on VM recreation. Update strategy Recreate
    
    Signed-off-by: makhov <amakhov@mirantis.com>
    
    * Fix ssh host key mismatch on VM recreation.
    
    Signed-off-by: makhov <amakhov@mirantis.com>
    
    ---------
    
    Signed-off-by: makhov <amakhov@mirantis.com>

--- internal/controller/infrastructure/ssh_provisioner.go
+++ internal/controller/infrastructure/ssh_provisioner.go
@@ -34,9 +34,19 @@ import (
 
 	api "github.com/k0sproject/k0smotron/api/infrastructure/v1beta2"
 	"github.com/k0sproject/k0smotron/internal/provisioner"
+	"github.com/k0sproject/rig/v2/protocol/ssh/hostkey"
 )
 
 var regex = regexp.MustCompile(`--kubelet-root-dir[ =](/[/a-zA-Z0-9_-]+)+`)
+
+func init() {
+	// RemoteMachine manages infrastructure where VMs may be reprovisioned with
+	// new host keys at the same address. Disable host key verification to avoid
+	// persistent connection failures after a wipe-and-reprovision cycle.
+	hostkey.KnownHostsPathFromEnv = func() (string, bool) {
+		return "/dev/null", true
+	}
+}
 
 // SSHProvisioner is responsible for provisioning a remote machine using SSH.
 type SSHProvisioner struct {
