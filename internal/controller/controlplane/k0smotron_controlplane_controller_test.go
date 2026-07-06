k0sproject/k0smotron

commit 211072e29aca5c325d40e60875a763f60ffe3fcb

Add clustercache for worklaod clusters connections
Signed-off-by: apedriza <adripedriza@gmail.com>

internal/controller/controlplane/k0smotron_controlplane_controller.go
@@ -38,8 +38,8 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/cluster-api/controllers/clustercache"
 	"sigs.k8s.io/cluster-api/controllers/external"
-	"sigs.k8s.io/cluster-api/controllers/remote"
 	crcontroller "sigs.k8s.io/controller-runtime/pkg/controller"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -83,6 +83,7 @@ var currentSpec kapi.ClusterSpec
 type K0smotronController struct {
 	client.Client
 	SecretCachingClient client.Client
+	ClusterCache        clustercache.ClusterCache
 	Scheme              *runtime.Scheme
 	ClientSet           *kubernetes.Clientset
 	RESTConfig          *rest.Config
@@ -187,7 +188,7 @@ func (c *K0smotronController) Reconcile(ctx context.Context, req ctrl.Request) (
 	defer func() {
 		derr := c.computeStatus(ctx, cluster, kcp, kmcScope)
 		if derr != nil {
-			if errors.Is(derr, ErrNotReady) {
+			if errors.Is(derr, util.ErrNotReady) {
 				res = ctrl.Result{RequeueAfter: 10 * time.Second, Requeue: true}
 			} else {
 				log.Error(derr, "Failed to update K0smotronControlPlane status")
@@ -593,7 +594,7 @@ func (c *K0smotronController) computeStatus(ctx context.Context, cluster *cluste
 	if unavailableReplicas > 0 ||
 		desiredVersion.String() != kcp.Status.Version ||
 		!conditions.IsTrue(kcp, string(cpv1beta2.ControlPlaneAvailableCondition)) {
-		return ErrNotReady
+		return util.ErrNotReady
 	}
 
 	return nil
@@ -635,9 +636,9 @@ func (c *K0smotronController) computeAvailability(ctx context.Context, cluster *
 	logger.Info("Pinging the workload cluster API")
 
 	// Get the CAPI cluster accessor
-	client, err := remote.NewClusterClient(ctx, "k0smotron", c.Client, capiutil.ObjectKey(cluster))
+	client, err := c.ClusterCache.GetClient(ctx, client.ObjectKeyFromObject(cluster))
 	if err != nil {
-		logger.Info("Failed to create cluster client", "error", err)
+		logger.Info("Failed to get cluster client", "error", err)
 		conditions.Set(kcp, metav1.Condition{
 			Type:    string(cpv1beta2.ControlPlaneAvailableCondition),
 			Status:  metav1.ConditionFalse,

internal/controller/util/wc_client.go
@@ -17,6 +17,7 @@ package util
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
@@ -25,25 +26,33 @@ import (
 
 	cpv1beta2 "github.com/k0sproject/k0smotron/api/controlplane/v1beta2"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/transport"
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
+	"sigs.k8s.io/cluster-api/controllers/clustercache"
 	"sigs.k8s.io/cluster-api/controllers/external"
 	"sigs.k8s.io/cluster-api/util/secret"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-// GetKubeClient returns a Kubernetes clientset for the given cluster.
-func GetKubeClient(ctx context.Context, hubClient client.Client, cluster *clusterv1.Cluster) (*kubernetes.Clientset, error) {
+var (
+	// ErrNotReady is used to indicate that the control plane is not ready yet.
+	ErrNotReady = fmt.Errorf("waiting for the state")
+)
+
+// GetWorkloadClusterClientset returns a Kubernetes clientset for the given cluster.
+func GetWorkloadClusterClientset(ctx context.Context, hubClient client.Client, cache clustercache.ClusterCache, cluster *clusterv1.Cluster) (*kubernetes.Clientset, error) {
 
 	k0sControlPlane, err := FindK0sControlPlane(ctx, hubClient, cluster)
 	if err != nil {
 		return nil, fmt.Errorf("failed to find K0sControlPlane: %w", err)
 	}
 
-	restConfig, err := getTunneledRestConfigIfPossible(ctx, hubClient, k0sControlPlane, client.ObjectKeyFromObject(cluster))
+	restConfig, err := getRESTConfig(ctx, hubClient, cache, k0sControlPlane, client.ObjectKeyFromObject(cluster))
 	if err != nil {
 		return nil, fmt.Errorf("failed to get rest config for cluster %s: %w", cluster.Name, err)
 	}
@@ -78,8 +87,8 @@ func GetKubeClient(ctx context.Context, hubClient client.Client, cluster *cluste
 
 // GetControllerRuntimeClient returns a controller-runtime client for the given cluster. It takes into account the possibility of the cluster accessing API server through a
 // tunnel, and in that case it will return a client that uses the tunnel to access the API server. If the cluster is not using a tunnel, it will return a regular client.
-func GetControllerRuntimeClient(ctx context.Context, hubClient client.Client, kcp *cpv1beta2.K0sControlPlane, cluster client.ObjectKey) (client.Client, error) {
-	restConfig, err := getTunneledRestConfigIfPossible(ctx, hubClient, kcp, cluster)
+func GetControllerRuntimeClient(ctx context.Context, hubClient client.Client, clustercache clustercache.ClusterCache, kcp *cpv1beta2.K0sControlPlane, cluster client.ObjectKey) (client.Client, error) {
+	restConfig, err := getRESTConfig(ctx, hubClient, clustercache, kcp, cluster)
 	if err != nil {
 		return nil, err
 	}
@@ -108,33 +117,29 @@ func FindK0sControlPlane(ctx context.Context, c client.Client, cluster *clusterv
 	return kcp, nil
 }
 
-func getTunneledRestConfigIfPossible(ctx context.Context, hubClient client.Client, cp *cpv1beta2.K0sControlPlane, cluster client.ObjectKey) (*rest.Config, error) {
-	if cp == nil || !cp.Spec.K0sConfigSpec.Tunneling.Enabled {
-		// If control plane is nil means that the control plane is not K0sControlPlane, but K0smotronControlPlane, which does not support tunneling and will
-		// always use the regular kubeconfig secret. Fallback to regular kubeconfig secret in case tunneling is not enabled.
-		return fromKubeconfigSecretToRestConfig(ctx, hubClient, client.ObjectKey{
-			Namespace: cluster.Namespace, // assuming the secret is in the same namespace as the cluster
-			Name:      secret.Name(cluster.Name, secret.Kubeconfig),
-		})
+func getRESTConfig(ctx context.Context, hubClient client.Client, cache clustercache.ClusterCache, kcp *cpv1beta2.K0sControlPlane, cluster client.ObjectKey) (*rest.Config, error) {
+	logger := log.FromContext(ctx)
+
+	if !isTunneledRestConfigPossible(kcp) {
+		restConfig, err := cache.GetRESTConfig(ctx, cluster)
+		if err != nil {
+			if errors.Is(err, clustercache.ErrClusterNotConnected) {
+				logger.Info("Connection to workload cluster is not established yet")
+				return nil, ErrNotReady
+			}
+			return nil, err
+		}
+
+		return restConfig, nil
 	}
 
-	// If worker is not enabled on the control-plane node, tunneled rest.Config cannot be used because a chicken-egg issue:
-	// 1: K0smotron controller cannot reach workload cluster k8s api until FRPClient is running because connection is done through it. If so, `controlplane.spec.initialized = true`.
-	// 2: FRPClient cannot run without a worker machine. It cannot be deployed on controller nodes if `--enable-worker` is not configured.
-	// 3. Infra provider needs to see `controlplane.spec.initialized == true` in order to create a worker machine where FRPClient will run.
-	// 4. BACK TO 1!
-	if !slices.Contains(cp.Spec.K0sConfigSpec.Args, "--enable-worker") {
-		return fromKubeconfigSecretToRestConfig(ctx, hubClient, client.ObjectKey{
-			Namespace: cluster.Namespace, // assuming the secret is in the same namespace as the cluster
-			Name:      secret.Name(cluster.Name, secret.Kubeconfig),
-		})
-	}
+	// Getting rest.Config for tunneled access.
 
 	var (
 		restConfig *rest.Config
 		err        error
 	)
-	switch cp.Spec.K0sConfigSpec.Tunneling.Mode {
+	switch kcp.Spec.K0sConfigSpec.Tunneling.Mode {
 	case "proxy":
 		restConfig, err = fromKubeconfigSecretToRestConfig(ctx, hubClient, client.ObjectKey{
 			Namespace: cluster.Namespace, // assuming the secret is in the same namespace as the cluster
@@ -147,19 +152,41 @@ func getTunneledRestConfigIfPossible(ctx context.Context, hubClient client.Clien
 		})
 	}
 	if err != nil {
+		if apierrors.IsNotFound(err) {
+			logger.Info("Kubeconfig secret not created yet for tunneled access")
+			return nil, fmt.Errorf("%w: %v", ErrNotReady, err)
+		}
+
 		return nil, fmt.Errorf("error getting rest config for cluster %s: %w", cluster.Name, err)
 	}
 
 	return restConfig, nil
 }
 
+// isTunneledRestConfigPossible checks if it's possible to use a tunneled rest.Config to access the workload cluster API server based on the control plane configuration.
+// If tunneling is not enabled or if worker mode is not enabled on the control-plane node, it returns false, indicating that a regular rest.Config should be used instead.
+func isTunneledRestConfigPossible(cp *cpv1beta2.K0sControlPlane) bool {
+	if cp == nil || !cp.Spec.K0sConfigSpec.Tunneling.Enabled {
+		// If control plane is nil means that the control plane is not K0sControlPlane, but K0smotronControlPlane, which does not support tunneling and will
+		// always use the regular kubeconfig secret. Fallback to regular kubeconfig secret in case tunneling is not enabled.
+		return false
+	}
+
+	// If worker is not enabled on the control-plane node, tunneled rest.Config cannot be used because a chicken-egg issue:
+	// 1: K0smotron controller cannot reach workload cluster k8s api until FRPClient is running because connection is done through it. If so, `controlplane.spec.initialized = true`.
+	// 2: FRPClient cannot run without a worker machine. It cannot be deployed on controller nodes if `--enable-worker` is not configured.
+	// 3. Infra provider needs to see `controlplane.spec.initialized == true` in order to create a worker machine where FRPClient will run.
+	// 4. BACK TO 1!
+	if !slices.Contains(cp.Spec.K0sConfigSpec.Args, "--enable-worker") {
+		return false
+	}
+
+	return true
+}
+
 func fromKubeconfigSecretToRestConfig(ctx context.Context, managementClusterClient client.Client, kubeconfig client.ObjectKey) (*rest.Config, error) {
 	kubeconfigSecret := &corev1.Secret{}
-	key := client.ObjectKey{
-		Namespace: kubeconfig.Namespace,
-		Name:      kubeconfig.Name,
-	}
-	err := managementClusterClient.Get(ctx, key, kubeconfigSecret)
+	err := managementClusterClient.Get(ctx, kubeconfig, kubeconfigSecret)
 	if err != nil {
 		return nil, err
 	}

.golangci.yml
@@ -69,7 +69,7 @@ linters:
     rules:
       - linters: [revive]
         text: "var-naming: avoid meaningless package names"
-        path: "(internal/controller|inttest)/util/"
+        path: "(internal/controller/util/|inttest/util/)"
     paths:
       # Ignore issues in generated files.
       - zz_generated.*\.go$

cmd/main.go
@@ -36,6 +36,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
+	"sigs.k8s.io/cluster-api/controllers/clustercache"
 	"sigs.k8s.io/cluster-api/util/flags"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
@@ -264,10 +265,33 @@ func main() {
 		MaxConcurrentReconciles: concurrency,
 	}
 
+	ctx := ctrl.SetupSignalHandler()
+
+	clusterCache, err := clustercache.SetupWithManager(ctx, mgr, clustercache.Options{
+		SecretClient: secretCachingClient,
+		Cache:        clustercache.CacheOptions{},
+		Client: clustercache.ClientOptions{
+			UserAgent: "k0smotron",
+			Cache: clustercache.ClientCacheOptions{
+				DisableFor: []client.Object{
+					// Don't cache ConfigMaps & Secrets.
+					&corev1.ConfigMap{},
+					&corev1.Secret{},
+				},
+			},
+		},
+	}, ctrlOptions)
+	if err != nil {
+		setupLog.Error(err, "Unable to create ClusterCache")
+		os.Exit(1)
+	}
+
 	if isControllerEnabled(bootstrapController) && runCAPIControllers {
+
 		if err = (&bootstrap.Controller{
 			Client:              mgr.GetClient(),
 			SecretCachingClient: secretCachingClient,
+			ClusterCache:        clusterCache,
 			Scheme:              mgr.GetScheme(),
 			ClientSet:           clientSet,
 			RESTConfig:          restConfig,
@@ -278,6 +302,7 @@ func main() {
 		if err = (&bootstrap.ControlPlaneController{
 			Client:              mgr.GetClient(),
 			SecretCachingClient: secretCachingClient,
+			ClusterCache:        clusterCache,
 			Scheme:              mgr.GetScheme(),
 			ClientSet:           clientSet,
 			RESTConfig:          restConfig,
@@ -300,6 +325,7 @@ func main() {
 			if err = (&controlplane.K0smotronController{
 				Client:              mgr.GetClient(),
 				SecretCachingClient: secretCachingClient,
+				ClusterCache:        clusterCache,
 				Scheme:              mgr.GetScheme(),
 				ClientSet:           clientSet,
 				RESTConfig:          restConfig,
@@ -311,6 +337,7 @@ func main() {
 			if err = (&controlplane.K0sController{
 				Client:              mgr.GetClient(),
 				SecretCachingClient: secretCachingClient,
+				ClusterCache:        clusterCache,
 				ClientSet:           clientSet,
 				RESTConfig:          restConfig,
 			}).SetupWithManager(mgr, ctrlOptions); err != nil {
@@ -368,9 +395,10 @@ func main() {
 	// as both of them create/update Machines.
 	if runCAPIControllers && (isControllerEnabled(infrastructureController) || isControllerEnabled(bootstrapController)) {
 		if err = (&bootstrap.ProviderIDController{
-			Client:    mgr.GetClient(),
-			Scheme:    mgr.GetScheme(),
-			ClientSet: clientSet,
+			Client:       mgr.GetClient(),
+			Scheme:       mgr.GetScheme(),
+			ClientSet:    clientSet,
+			ClusterCache: clusterCache,
 		}).SetupWithManager(mgr, ctrlOptions); err != nil {
 			setupLog.Error(err, "unable to create controller", "controller", "ProviderID")
 			os.Exit(1)
@@ -387,7 +415,7 @@ func main() {
 	}
 
 	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}

internal/controller/bootstrap/controlplane_bootstrap_controller.go
@@ -41,6 +41,7 @@ import (
 	kubeadmbootstrapv1 "sigs.k8s.io/cluster-api/api/bootstrap/kubeadm/v1beta2"
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 	bsutil "sigs.k8s.io/cluster-api/bootstrap/util"
+	"sigs.k8s.io/cluster-api/controllers/clustercache"
 	"sigs.k8s.io/cluster-api/controllers/external"
 	capiutil "sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/cluster-api/util/annotations"
@@ -65,6 +66,7 @@ import (
 type ControlPlaneController struct {
 	client.Client
 	SecretCachingClient client.Client
+	ClusterCache        clustercache.ClusterCache
 	Scheme              *runtime.Scheme
 	ClientSet           *kubernetes.Clientset
 	RESTConfig          *rest.Config
@@ -465,7 +467,7 @@ func (c *ControlPlaneController) genControlPlaneJoinFiles(ctx context.Context, s
 		return nil, fmt.Errorf("failed to get K0sControlPlane resource: %w", err)
 	}
 
-	chCS, err := util.GetControllerRuntimeClient(ctx, c.Client, cp, client.ObjectKeyFromObject(scope.Cluster))
+	chCS, err := util.GetControllerRuntimeClient(ctx, c.Client, c.ClusterCache, cp, client.ObjectKeyFromObject(scope.Cluster))
 	if err != nil {
 		log.Error(err, "Failed to getting child cluster client set")
 		return nil, err

internal/controller/bootstrap/providerid_controller.go
@@ -29,6 +29,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/util/retry"
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
+	"sigs.k8s.io/cluster-api/controllers/clustercache"
 	capiutil "sigs.k8s.io/cluster-api/util"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -41,8 +42,9 @@ import (
 // ProviderIDController is responsible for reconciling the ProviderID field of the Machine resource.
 type ProviderIDController struct {
 	client.Client
-	Scheme    *runtime.Scheme
-	ClientSet *kubernetes.Clientset
+	Scheme       *runtime.Scheme
+	ClientSet    *kubernetes.Clientset
+	ClusterCache clustercache.ClusterCache
 }
 
 // Reconcile reconciles the ProviderID field of the Machine resource and
@@ -82,7 +84,7 @@ func (p *ProviderIDController) Reconcile(ctx context.Context, req ctrl.Request) 
 		return ctrl.Result{}, fmt.Errorf("can't get cluster %s/%s: %w", machine.Namespace, machine.Spec.ClusterName, err)
 	}
 
-	childClient, err := k0smoutil.GetKubeClient(context.Background(), p.Client, cluster)
+	childClient, err := k0smoutil.GetWorkloadClusterClientset(ctx, p.Client, p.ClusterCache, cluster)
 	if err != nil {
 		return ctrl.Result{}, fmt.Errorf("can't get kube client for cluster %s/%s: %w. may not be created yet", machine.Namespace, machine.Spec.ClusterName, err)
 	}

internal/controller/bootstrap/worker_bootstrap_controller.go
@@ -34,6 +34,7 @@ import (
 	"k8s.io/utils/ptr"
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 	bsutil "sigs.k8s.io/cluster-api/bootstrap/util"
+	"sigs.k8s.io/cluster-api/controllers/clustercache"
 	"sigs.k8s.io/cluster-api/controllers/external"
 	capiutil "sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/cluster-api/util/annotations"
@@ -65,6 +66,7 @@ const (
 type Controller struct {
 	client.Client
 	SecretCachingClient client.Client
+	ClusterCache        clustercache.ClusterCache
 	Scheme              *runtime.Scheme
 	ClientSet           *kubernetes.Clientset
 	RESTConfig          *rest.Config
@@ -364,7 +366,7 @@ func (r *Controller) getK0sToken(ctx context.Context, scope *Scope) (string, err
 			return "", fmt.Errorf("failed to get K0sControlPlane resource: %w", err)
 		}
 
-		wcClient, err = util.GetControllerRuntimeClient(ctx, r.Client, cp, client.ObjectKeyFromObject(scope.Cluster))
+		wcClient, err = util.GetControllerRuntimeClient(ctx, r.Client, r.ClusterCache, cp, client.ObjectKeyFromObject(scope.Cluster))
 		if err != nil {
 			return "", fmt.Errorf("failed to create child cluster client: %w", err)
 		}

internal/controller/controlplane/helper.go
@@ -506,10 +506,11 @@ func (c *K0sController) markChildControlNodeToLeave(ctx context.Context, name st
 }
 
 func (c *K0sController) deleteOldControlNodes(ctx context.Context, cluster *clusterv1.Cluster) error {
-	kubeClient, err := c.getKubeClient(ctx, cluster)
+	kubeClient, err := c.getWorkloadClusterClientset(ctx, cluster)
 	if err != nil {
-		return fmt.Errorf("error getting kube client: %w", err)
+		return fmt.Errorf("error getting workload cluster client: %w", err)
 	}
+
 	machines, err := collections.GetFilteredMachinesForCluster(ctx, c, cluster, collections.ControlPlaneMachines(cluster.Name))
 	if err != nil {
 		return fmt.Errorf("error getting all machines: %w", err)
@@ -589,7 +590,7 @@ func (c *K0sController) createAutopilotPlan(ctx context.Context, kcp *cpv1beta2.
 			// it is necessary to check if the current autopilot process corresponds to a previous update by comparing the current
 			// version of the resource with the desired one. If that is the case, the state is not yet ready to proceed with a new plan.
 			if version != kcp.Spec.Version {
-				return fmt.Errorf("previous autopilot is not finished: %w", ErrNotReady)
+				return fmt.Errorf("previous autopilot is not finished: %w", util.ErrNotReady)
 			}
 
 			return nil

commit f483c3f8396b99ebd7a68da77fb9cd910759856c

fix(controlplane): break hosted CP deadlock
K0smotronControlPlane's availability ping goes through ClusterCache,
which won't connect until infrastructureProvisioned=true. When infra
is externally managed (cluster.x-k8s.io/managed-by: k0smotron),
k0smotron alone patches InfraCluster.status.ready and gates that patch
on ControlPlaneInitialized: set only after a successful ping.
Permanent deadlock.

Fall back to a direct CAPI kubeconfig-secret client on
ErrClusterNotConnected; the hosted CP is reachable regardless of
infra state.

Regressed by 211072e2.
Refs: k0rdent/kcm#2884

Signed-off-by: Michael Morgen <mmorgen@posteo.com>

internal/controller/controlplane/k0smotron_controlplane_controller.go
@@ -646,17 +646,42 @@ func (c *K0smotronController) computeAvailability(ctx context.Context, cluster *
 	// and checking if the control plane is initialized
 	logger.Info("Pinging the workload cluster API")
 
+	clusterKey := client.ObjectKeyFromObject(cluster)
+
 	// Get the CAPI cluster accessor
-	client, err := c.ClusterCache.GetClient(ctx, client.ObjectKeyFromObject(cluster))
+	client, err := c.ClusterCache.GetClient(ctx, clusterKey)
 	if err != nil {
-		logger.Info("Failed to get cluster client", "error", err)
-		conditions.Set(kcp, metav1.Condition{
-			Type:    string(cpv1beta2.ControlPlaneAvailableCondition),
-			Status:  metav1.ConditionFalse,
-			Reason:  "ClusterClientCreationFailed",
-			Message: err.Error(),
-		})
-		return
+		// ClusterCache refuses to connect until Cluster.status.initialization.infrastructureProvisioned=true.
+		// For K0smotronControlPlane the hosted CP runs in the mgmt cluster and its API is reachable via the
+		// CAPI kubeconfig secret regardless of infra state; when infra is externally managed by k0smotron
+		// itself with cluster.x-k8s.io/managed-by: k0smotron, that ClusterCache gate
+		// and the ControlPlaneInitialized->status.ready patch chain form a permanent deadlock, see k0rdent/kcm#2884.
+		// Fall back to a direct kubeconfig-secret client on ErrClusterNotConnected to break it
+		if errors.Is(err, clustercache.ErrClusterNotConnected) {
+			logger.Info("ClusterCache not connected yet, falling back to direct kubeconfig-secret client")
+			directClient, fbErr := util.GetWorkloadClusterClientFromKubeconfigSecret(ctx, c.Client, clusterKey)
+			if fbErr != nil {
+				logger.Error(fbErr, "Failed to build direct workload cluster client fallback")
+				conditions.Set(kcp, metav1.Condition{
+					Type:    cpv1beta2.ControlPlaneAvailableCondition,
+					Status:  metav1.ConditionFalse,
+					Reason:  "ClusterClientCreationFailed",
+					Message: fbErr.Error(),
+				})
+				return
+			}
+
+			client = directClient
+		} else {
+			logger.Error(err, "Failed to get cluster client")
+			conditions.Set(kcp, metav1.Condition{
+				Type:    cpv1beta2.ControlPlaneAvailableCondition,
+				Status:  metav1.ConditionFalse,
+				Reason:  "ClusterClientCreationFailed",
+				Message: err.Error(),
+			})
+			return
+		}
 	}
 
 	pingCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
@@ -752,7 +777,6 @@ func (c *K0smotronController) patchInfrastructureStatus(ctx context.Context, clu
 
 // reconcileDelete handles the deletion of the K0smotronControlPlane object when the controlplane replicas runs in a different cluster.
 func (c *K0smotronController) reconcileDelete(ctx context.Context, key types.NamespacedName, kcp *cpv1beta2.K0smotronControlPlane) (res ctrl.Result, err error) {
-
 	defer func() {
 		if err != nil && apierrors.IsNotFound(err) {
 			// The cluster is already deleted. Remove the finalizer from the K0smotronControlPlane object for complete cleanup.

internal/controller/util/wc_client.go
@@ -39,14 +39,11 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-var (
-	// ErrNotReady is used to indicate that the control plane is not ready yet.
-	ErrNotReady = fmt.Errorf("waiting for the state")
-)
+// ErrNotReady is used to indicate that the control plane is not ready yet.
+var ErrNotReady = fmt.Errorf("waiting for the state")
 
 // GetWorkloadClusterClientset returns a Kubernetes clientset for the given cluster.
 func GetWorkloadClusterClientset(ctx context.Context, hubClient client.Client, cache clustercache.ClusterCache, cluster *clusterv1.Cluster) (*kubernetes.Clientset, error) {
-
 	k0sControlPlane, err := FindK0sControlPlane(ctx, hubClient, cluster)
 	if err != nil {
 		return nil, fmt.Errorf("failed to find K0sControlPlane: %w", err)
@@ -90,6 +87,27 @@ func GetWorkloadClusterClientset(ctx context.Context, hubClient client.Client, c
 func GetControllerRuntimeClient(ctx context.Context, hubClient client.Client, clustercache clustercache.ClusterCache, kcp *cpv1beta2.K0sControlPlane, cluster client.ObjectKey) (client.Client, error) {
 	restConfig, err := getRESTConfig(ctx, hubClient, clustercache, kcp, cluster)
 	if err != nil {
+		return nil, err
+	}
+
+	return client.New(restConfig, client.Options{Scheme: hubClient.Scheme()})
+}
+
+// GetWorkloadClusterClientFromKubeconfigSecret returns a controller-runtime client for the given cluster built
+// directly from the CAPI-managed kubeconfig secret, bypassing ClusterCache. Intended as a fallback for callers whose
+// workload API is reachable independently of Cluster.status.initialization.infrastructureProvisioned, i.e.
+// K0smotronControlPlane, whose hosted CP runs as pods in the mgmt cluster. Wraps ErrNotReady when the kubeconfig
+// secret does not exist yet.
+func GetWorkloadClusterClientFromKubeconfigSecret(ctx context.Context, hubClient client.Client, cluster client.ObjectKey) (client.Client, error) {
+	restConfig, err := fromKubeconfigSecretToRestConfig(ctx, hubClient, client.ObjectKey{
+		Namespace: cluster.Namespace,
+		Name:      secret.Name(cluster.Name, secret.Kubeconfig),
+	})
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil, fmt.Errorf("%w: %v", ErrNotReady, err)
+		}
+
 		return nil, err
 	}
 

internal/controller/controlplane/k0smotron_controlplane_controller_test.go
@@ -22,6 +22,7 @@ import (
 	"fmt"
 	"testing"
 
+	cpv1beta2 "github.com/k0sproject/k0smotron/v2/api/controlplane/v1beta2"
 	kapi "github.com/k0sproject/k0smotron/v2/api/k0smotron.io/v1beta2"
 	"github.com/k0sproject/version"
 	"github.com/stretchr/testify/require"
@@ -30,6 +31,10 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
+	"sigs.k8s.io/cluster-api/controllers/clustercache"
+	"sigs.k8s.io/cluster-api/util/conditions"
+	"sigs.k8s.io/cluster-api/util/secret"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
@@ -328,7 +333,6 @@ func TestIsClusterSpecSynced(t *testing.T) {
 			})
 		}
 	}
-
 }
 
 func Test_alignToSpecVersionFormat(t *testing.T) {
@@ -362,6 +366,99 @@ func Test_alignToSpecVersionFormat(t *testing.T) {
 			got, err := alignToSpecVersionFormat(tt.specVersion, tt.currentVersion)
 			require.NoError(t, err)
 			require.True(t, tt.want.Equal(got), "alignToSpecVersionFormat() = %v, want %v", got, tt.want)
+		})
+	}
+}
+
+// Test_computeAvailability regression-guards k0rdent/kcm#2884
+func Test_computeAvailability(t *testing.T) {
+	const (
+		clusterName = "hosted"
+		clusterNs   = "default"
+	)
+
+	// syntactically valid kubeconfig pointing to an unroutable endpoint; the fallback only builds a client
+	// from this blob, the subsequent ping is expected to fail and that is exactly what distinguishes
+	// fallback success from a pre-ping ClusterClientCreationFailed
+	kubeconfigYAML := []byte(`apiVersion: v1
+kind: Config
+clusters:
+- name: test
+  cluster:
+    server: https://127.0.0.1:1
+    insecure-skip-tls-verify: true
+contexts:
+- name: test
+  context:
+    cluster: test
+    user: test
+current-context: test
+users:
+- name: test
+  user:
+    token: dummy
+`)
+
+	kubeconfigSecret := &corev1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      secret.Name(clusterName, secret.Kubeconfig),
+			Namespace: clusterNs,
+		},
+		Data: map[string][]byte{
+			secret.KubeconfigDataName: kubeconfigYAML,
+		},
+	}
+
+	scheme := runtime.NewScheme()
+	require.NoError(t, corev1.AddToScheme(scheme))
+	require.NoError(t, clusterv1.AddToScheme(scheme))
+	require.NoError(t, cpv1beta2.AddToScheme(scheme))
+
+	tests := []struct {
+		name          string
+		hubObjects    []client.Object
+		wantCondition string
+	}{
+		{
+			name:          "fallback client used when ClusterCache is not connected and kubeconfig secret exists",
+			hubObjects:    []client.Object{kubeconfigSecret},
+			wantCondition: "KubeSystemNamespaceNotAccessible",
+		},
+		{
+			name:          "ClusterClientCreationFailed surfaced when kubeconfig secret is missing",
+			hubObjects:    nil,
+			wantCondition: "ClusterClientCreationFailed",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cluster := &clusterv1.Cluster{
+				ObjectMeta: v1.ObjectMeta{Name: clusterName, Namespace: clusterNs},
+			}
+
+			hubClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(tt.hubObjects...).Build()
+
+			// non-matching cluster key -> clustercache.ErrClusterNotConnected on GetClient (kcm#2884)
+			disconnectedCache := clustercache.NewFakeClusterCache(
+				fake.NewClientBuilder().Build(),
+				client.ObjectKey{Name: "unrelated", Namespace: "unrelated"},
+			)
+
+			c := &K0smotronController{
+				Client:       hubClient,
+				ClusterCache: disconnectedCache,
+				Scheme:       scheme,
+			}
+			kcp := &cpv1beta2.K0smotronControlPlane{
+				ObjectMeta: v1.ObjectMeta{Name: clusterName, Namespace: clusterNs},
+			}
+
+			c.computeAvailability(context.Background(), cluster, kcp)
+
+			cond := conditions.Get(kcp, string(cpv1beta2.ControlPlaneAvailableCondition))
+			require.NotNil(t, cond, "Available condition must be set")
+			require.Equal(t, tt.wantCondition, cond.Reason)
 		})
 	}
 }