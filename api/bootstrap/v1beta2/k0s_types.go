11c94a625cca5549727b18720221197638de5744
s3rj1k <evasive.gyron@gmail.com>
2026-09-10 15:09:03 +0000

    Name preK0sCommands in the v1beta2 download URL note
    The note pointed at preStartCommands, a v1beta1 field that does not exist in v1beta2, and the wrong name reached
    the reference docs, the CRD descriptions that kubectl explain prints, and one hand written page.
    
    Signed-off-by: s3rj1k <evasive.gyron@gmail.com>

--- api/bootstrap/v1beta2/k0s_types.go
+++ api/bootstrap/v1beta2/k0s_types.go
@@ -363,7 +363,7 @@ type K0sConfigSpec struct {
 	// If 'oci' schema is used and the OCI registry requires authentication, make sure to set up the authentication beforehand
 	// by adding a file to the Files section that contains the necessary config for ORAS. See: https://oras.land/docs/how_to_guides/authentication/
 	// The file must be placed at `/root` directory (HOME for cloud-init execution time) and named `config.json`.
-	// NOTE: use `.preStartCommands` to set DOCKER_CONFIG environment variable in order to let ORAS pick up your custom config file.
+	// NOTE: use `.preK0sCommands` to set DOCKER_CONFIG environment variable in order to let ORAS pick up your custom config file.
 	// When no value is specified, k0smotron will use the default URL: https://get.k0s.sh
 	// +kubebuilder:validation:Optional
 	DownloadURL string `json:"downloadURL,omitempty"`
